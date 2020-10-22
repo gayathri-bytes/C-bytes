77ab1718a45916cd8a23a9e998a05a9d0c1ad77c
gayathri-bytes <73304225+gayathri-bytes@users.noreply.github.com>
2020-10-22 23:19:22 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void main()
+void main(int argc, char *argv[])
 {
   int i, n;
   int n1 = 0;
   int n2 = 1;
   int n3;
-  printf("Enter the number of terms: ");
+  n = atoi(argv[1]);
   scanf("%d", &n);
-  printf("The respective fibonacci series is: ");
+  printf("The respective fibonacci series is: 0 1 ");
  
-  for( i =1; i<=n; i++)
+  for( i =3; i<=n; i++)
   {
-    printf("%d, ", n1);
     n3 = n1 + n2;
+    printf("%d ", n3);
     n1 = n2;
     n2 = n3;                                      
-  
   }
   
 }
-
