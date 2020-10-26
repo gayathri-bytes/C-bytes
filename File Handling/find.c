681f75510c12487c99854eb16e4fe038a1bf171e
gayathri-bytes <73304225+gayathri-bytes@users.noreply.github.com>
2020-10-26 21:46:28 +0530

    Update find.c

--- File Handling/find.c
+++ File Handling/find.c
@@ -1,73 +1,67 @@
 //C Program to find and replace a string in a file
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUF_SIZE 100
 
-void main(int argc, char *argv[])
-
+int main(int argc, char *argv[])
 {
- FILE *iptr, *optr;
- char dart;
- char replaceWord[100], readWord[100];
- iptr = fopen("file.txt", "r");
- optr = fopen("ofile.txt", "w+");
- if ((iptr == NULL) || (optr == NULL))
- {
-   printf("File doesn't exist ");
- }
- printf("The input file: \n");
-   while(1)
-   {
-     dart = fgetc(iptr);
-     if (dart == EOF)
-     { 
-       break;
-     }
-     else
-     {
-       printf("%c", dart);
-     }
-   }
-   
-  char* findWord = argv[1];
-   
-  printf("Enter the word to replace with: ");
-  fgets(replaceWord, 100, stdin);
-   
-  replaceWord[strlen(replaceWord) - 1] = replaceWord[strlen(replaceWord)]; 
-  
-  
-  rewind(iptr); 
-  while (!feof(iptr))
-    { 
-  
-        fscanf(iptr, "%s", readWord); 
-  
-        if (strcmp(readWord, findWord) == 0) 
-        { 
-  
-            strcpy(readWord, replaceWord); 
-        } 
- 
-        fprintf(optr, "%s ", readWord); 
-    } 
-  
-  rewind(optr); 
-  while (1)
-    { 
-        dart = fgetc(optr); 
-        if (dart == EOF) 
-        { 
-            break; 
-        } 
-        printf("%c", dart); 
-    } 
-   
-  fclose(iptr); 
-  fclose(optr);
-  
+	FILE *iptr, *optr;
+	char dart;
+	char replaceword[BUF_SIZE], readword[BUF_SIZE];
+	char *findword;
+
+	if (argc < 2) {
+		printf ("Please input the find string\n");
+		return -1;
+	}
+	findword = argv[1];
+	iptr = fopen("file.txt", "r");
+	optr = fopen("ofile.txt", "w+");
+	if ((iptr == NULL) || (optr == NULL)) {
+		printf("File doesn't exist ");
+		return -1;
+	}
+	printf("The input file: \n");
+	while (1) {
+		dart = fgetc(iptr);
+		if (dart == EOF)
+		{ 
+			break;
+		} else {
+			printf("%c", dart);
+		}
+	}
+	printf("Enter the word to replace with: ");
+	fgets(replaceword, 100, stdin);
+
+	replaceword[strlen(replaceword) - 1] =
+			replaceword[strlen(replaceword)]; 
+	rewind(iptr); 
+	while (!feof(iptr)) { 
+		fscanf(iptr, "%s", readword); 
+		if (strcmp(readword, findword) == 0) { 
+			strcpy(readword, replaceword); 
+		} 
+		fprintf(optr, "%s ", readword); 
+	} 
+	rewind(optr); 
+	while (1) { 
+		dart = fgetc(optr); 
+		if (dart == EOF) { 
+			break; 
+		} 
+		printf("%c", dart); 
+	} 
+
+	fclose(iptr); 
+	fclose(optr);
+	return 0;
 }
+
+
+
 
 
 
