bc5ffbc90b5d819b16ccda69defcdab76f0c0754
gayathri-bytes <73304225+gayathri-bytes@users.noreply.github.com>
2020-10-28 12:05:24 +0530

    Update format.c

--- file_removeTrailingSpaces/format.c
+++ file_removeTrailingSpaces/format.c
@@ -7,26 +7,24 @@
 
 void main()
 {
-FILE *ipt, *opt;
-char str[BUFFER_SIZE];
-ipt = fopen("sample.txt", "r");
-opt = fopen("out.txt", "w");
-if ((ipt == NULL) || (opt == NULL)
-   printf("File doesn't exist ");
-while (fgets(str, BUFFER_SIZE, ipt) != NULL) {
-      int index, i;
-      index = -1;
-      i = 0;
-      while (str[i] != '\0') {
-            if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-                     index= i;
-            i++;
-}
-      str[index + 1] = '\0';
-      fprintf(opt, "%s\n", str); 
-}
-fclose(ipt);
-fclose(opt);  
+         FILE *ipt, *opt;
+         char str[BUFFER_SIZE];
+         ipt = fopen("sample.txt", "r");
+         opt = fopen("out.txt", "w");
+         if ((ipt == NULL) || (opt == NULL)
+                printf("File doesn't exist ");
+         while (fgets(str, BUFFER_SIZE, ipt) != NULL) {
+                 int index = -1, i = 0;
+                 while (str[i] != '\0') {
+                         if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+                                index= i;
+                         i++;
+                 }
+                 str[index + 1] = '\0';
+                 fprintf(opt, "%s\n", str); 
+          }
+          fclose(ipt);
+          fclose(opt);  
 }
 
 
